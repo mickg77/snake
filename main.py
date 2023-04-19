b7217d12fdfc65d81e7edea79b62d8141f4cc9a2
mickg77 <mickg77@hotmail.com>
2023-04-19 15:27:23 +0100

    snake game with snake object created

--- main.py
+++ main.py
@@ -3,15 +3,22 @@
 import time
 from snake import Snake
 from turtle import Screen
-
+from fruit import Food
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
+snake= Snake() # create the instance of the Snake
+food = Food() # create instance of Food
 
-snake= Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 
 game_is_on = True
 
@@ -19,6 +26,8 @@ while game_is_on:
     time.sleep(0.1)  # adds 0.1 sec delay
     screen.update()  # update screen
     snake.move()
-
+    if snake.head.distance(food)< 15:
+        print("yum")
+        food.refresh()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -2,12 +2,20 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20 # How far snake will move
+
+#  Saves us dealing with angles by defining directions as constants
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
 
     def __init__(self):
-
+        # initialise the snake
         self.segments = []
         self.create_snake()
+        self.head =self.segments[0]
 
     def create_snake(self):
 
@@ -25,4 +33,20 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             #  set the co-ords of the further block to the one behind
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
